7f105a199591c1ffe8f25fb9ef6202d91505ca41
Daniela Castillo <152355250+ddnyzz@users.noreply.github.com>
2026-03-25 22:20:53 -0600

    Delete server.js

--- server.js
+++
@@ -1,8 +0,0 @@
-    require("dotenv").config();
-    const { createApp } = require("./src/app");
-
-    const app = createApp();
-
-    app.listen(process.env.PORT || 3000, () => {
-    console.log("Biblioteca en puerto 3000");
-    });
